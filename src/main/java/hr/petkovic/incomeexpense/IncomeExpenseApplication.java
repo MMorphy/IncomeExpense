b138c1b9294972d9ae017cdebfa86b7ac01c6a61
MMorphy <tomislav.petkovic@cromtg.com>
2019-12-23 12:12:24 +0100

    Project init

---
+++ src/main/java/hr/petkovic/incomeexpense/IncomeExpenseApplication.java
@@ -0,0 +1,13 @@
+package hr.petkovic.incomeexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeExpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/petkovic/incomeexpense/IncomeExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.petkovic.incomeexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
